08c9c5ae8890456c6c4d12485c96e33853b88d05
David Agam <davidght1@gmail.com>
2023-06-17 20:51:42 +0300

    modified:   controllers/productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,22 +1,31 @@
+// imports
+const Product = require("../models/productModel");
+
 //get all products
 const getProducts = async (req, res) => {
-    res.send({message: "get all products"})
+  try {
+    const products = await Product.find();
+
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: "Failed to retrieve products" });
+  }
 };
 
-// update a product
+// update a product stock
 const updateProduct = async (req, res) => {
-    res.send({message: "update products"})
+  res.send({ message: "update products" });
 };
 
 //for admins only
 
 // add a product
 const addProduct = async (req, res) => {
-    res.send({message: "add product"})
+  res.send({ message: "add product" });
 };
 // delete a product
 const deleteProduct = async (req, res) => {
-    res.send({message: "delete products"})
+  res.send({ message: "delete products" });
 };
 
 module.exports = { getProducts, updateProduct, addProduct, deleteProduct };
